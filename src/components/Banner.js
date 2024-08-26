26204e5810bb633567f0eddae906f8e6a9bf4884
Shxblvrbs <131173161+Shxblvrbs@users.noreply.github.com>
2024-08-26 01:32:40 -0400

    Update Banner.js
    test 3 for spacing

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -13,7 +13,7 @@ import { fadeIn } from '../variants';
 const Banner = () => {
   return (
     <section
-      className='min-h-[85vh] lg:min-h-[78vh] flex items-center md:-mt-8'
+      className='min-h-[85vh] lg:min-h-[78vh] flex items-center lg:-mt-0 -mt-8'
       id='home'
     >
       <div className='container mx-auto mb-14'>
