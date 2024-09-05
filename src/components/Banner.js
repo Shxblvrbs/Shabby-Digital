611622aa6e8256bbd16ea980a3962ec48dfc9b1b
Shxblvrbs <131173161+Shxblvrbs@users.noreply.github.com>
2024-09-05 19:06:06 -0400

    Update Banner.js
    "Schedule a Call to get a FREE Website Today!"

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -69,7 +69,7 @@ const Banner = () => {
               viewport={{ once: false, amount: 0.7 }}
               className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'
             >
-              <button className='btn btn-lg'><a href="https://link.shabbydigital.com/widget/booking/7tCsfKqjOw2CMtCXmUkh">Book a Free Website Call!</a></button>
+              <button className='btn btn-lg'><a href="https://link.shabbydigital.com/widget/booking/7tCsfKqjOw2CMtCXmUkh">Schedule a Call to get a FREE Website Today!</a></button>
               <a href='#' className='text-gradient btn-link'>
                 Learn More
               </a>
